ca1b97b4e681f6f91220c89ae3ac06adee0e9b58
Srinath Janakiraman <me@vjsrinath.com>
2017-11-16 02:17:15 +0530

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # egm
 Express Gateway Manager
+
+
+A Super Simple GUI to manage Express Gateway Instances
--- src/EGM.WebClient/ClientApp/components/NavMenu.tsx
+++ src/EGM.WebClient/ClientApp/components/NavMenu.tsx
@@ -20,17 +20,7 @@ export class NavMenu extends React.Component<{}, {}> {
                             <NavLink exact to={ '/' } activeClassName='active'>
                                 <span className='glyphicon glyphicon-home'></span> Home
                             </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={ '/counter' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-education'></span> Counter
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={ '/fetchdata' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Fetch data
-                            </NavLink>
-                        </li>
+                        </li>                        
                     </ul>
                 </div>
             </div>
